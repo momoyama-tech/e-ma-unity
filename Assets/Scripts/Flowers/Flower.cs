66924584a9dfeed36cf92723d2712398415d5f06
kdix-23-0240 <2312110240g@kindai.ac.jp>
2025-02-28 21:59:31 +0900

    マテリアルの動的変更

--- Assets/Scripts/Flowers/Flower.cs
+++ Assets/Scripts/Flowers/Flower.cs
@@ -1,9 +1,22 @@
-// using UnityEngine;
+using UnityEngine;
 
-// public class Flower : MonoBehaviour
-// {
-//     void Start()
-//     {
-//         this.
-//     }
-// }
+public class Flower : MonoBehaviour
+{
+    [SerializeField] private Material[] materials;
+    private int index;
+
+    void Update()
+    {
+        index = 0;
+    }
+
+    void ChangeMaterial()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            
+            GetComponent<Renderer>().material = materials[index];
+            index++;
+        }
+    }
+}
