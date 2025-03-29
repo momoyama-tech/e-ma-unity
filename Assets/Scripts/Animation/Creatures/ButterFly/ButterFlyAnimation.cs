16824cef03565a638eb9cb5a8750d4754f67d4e4
kdix-23-0240 <2312110240g@kindai.ac.jp>
2025-03-29 11:32:22 +0900

    蝶の羽ばたきアニメーション実装(バグあり)

--- Assets/Scripts/Animation/Creatures/ButterFly/ButterFlyAnimation.cs
+++ Assets/Scripts/Animation/Creatures/ButterFly/ButterFlyAnimation.cs
@@ -13,16 +13,23 @@ public class ButterFlyAnimation : MonoBehaviour
     [SerializeField] private float _targetPosZ = 0;// 目的地Z
     [SerializeField] private float _butterFlySize = 1;// 蝶の大きさ
     [SerializeField] private float _flapHeight = 1;// 羽ばたく高さ
-    [SerializeField] private float _lifeTime = 5;// 羽ばたく時間
+    [SerializeField] private float _flapSpeed = 1;// 羽ばたく速度
+    [SerializeField] private float _lifeTime = 5;// 消滅までの時間
     [SerializeField] private float _flapNumPerSecond = 1;// 1秒間に羽ばたく回数
+    [SerializeField] private bool _isMove = false;// 移動するかどうか
+    [SerializeField] private bool _isSizeUp = false;// 大きくなるかどうか
+    [SerializeField] private bool _isFlap = false;// 羽ばたくかどうか
+    [SerializeField] private bool _isRotate = false;// 回転するかどうか
 
-    private Vector3 _bornPos;
-    private Vector3 _targetPos;
+    private Vector3 _bornPos;// 出現座標
+    private Vector3 _targetPos;// 目標座標
+    private Vector3 _moveDistance;// 出現位置と目的地の距離
 
     async Task Start()
     {
         _bornPos = new Vector3(_bornPosX, _bornPosY, _bornPosZ);
         _targetPos = new Vector3(_targetPosX, _targetPosY, _targetPosZ);
+        _moveDistance = _targetPos - _bornPos;
         transform.position = _bornPos;
         transform.localScale = new Vector3(0.01f, 0.01f, 0.01f);
         transform.DOScale(_butterFlySize, 1f).SetEase(Ease.Linear).WaitForCompletion();
@@ -33,11 +40,15 @@ public class ButterFlyAnimation : MonoBehaviour
     {
         Move();
         SizeUp();
-        // Flap();
+        Flap();
     }
 
     private void Move()
     {
+        if (_isMove == false)
+        {
+            return;
+        }
         // 目的地に向かって移動
         transform.DOMove(_targetPos, _lifeTime).SetEase(Ease.Linear).OnComplete(() =>
         {
@@ -54,24 +65,53 @@ public class ButterFlyAnimation : MonoBehaviour
     /// </summary>
     private void SizeUp()
     {
+        if (_isSizeUp == false)
+        {
+            return;
+        }
         float distance = Vector3.Distance(_bornPos, _targetPos);
         float sizeUpTime = distance / _lifeTime;
         transform.DOScale(_butterFlySize, sizeUpTime).SetEase(Ease.Linear);
     }
+
+    /// <summary>
+    /// 羽ばたきアニメーション
+    /// 移動距離と_lifeTimeを使って羽ばたく回数を決定
+    /// 1回に羽ばたく回数はランダムで決定
+    /// バグ：上下移動のアニメーションで上に移動したあと、y座標の更新が行われていない
+    /// そのため、下方向に移動した時、上に上がる前の座標から移動してしまう
+    /// そのため、上に移動した後、y座標を更新する必要がある
+    /// 移動がないと正常に動く
+    /// </summary>
+    /// <returns></returns>.
     private async UniTask Flap()
     {
-        for (int i = 0; i < _flapNumPerSecond; i++)
+        if (_isFlap == false)
+        {
+            return;
+        }
+        // 移動距離と_lifeTimeを使って羽ばたく回数を決定
+        for (int i = 0; i < (_moveDistance.magnitude / _lifeTime) * _flapNumPerSecond; i++)
         {
             // ランダムで羽ばたく回数を決定
             int randFlapNum = Random.Range(1, 3);
             for (int j = 0; j < randFlapNum; j++)
             {
-                transform.DOMoveY(_flapHeight, _lifeTime / 2).SetEase(Ease.Linear).OnComplete(() =>
-                {
-                transform.DOMoveY(0, _lifeTime / 2).SetEase(Ease.Linear);
-                });
+                // なめらかな上下移動
+                await transform.DOMoveY(this.transform.position.y + _flapHeight, _flapSpeed)
+                    .SetEase(Ease.InOutSine) // なめらかな動き
+                    .AsyncWaitForCompletion();
+
+                await transform.DOMoveY(this.transform.position.y - _flapHeight, _flapSpeed)
+                    .SetEase(Ease.InOutSine) // なめらかな動き
+                    .AsyncWaitForCompletion();
             }
             await UniTask.Delay((int)(_lifeTime * 1000 / _flapNumPerSecond));
         }
+    }
+
+    private void Rotation()
+    {
+
     }
 }
