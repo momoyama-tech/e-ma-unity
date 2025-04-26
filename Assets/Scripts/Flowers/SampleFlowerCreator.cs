f8e7128aa9aea24e372d1eb1cd16a39c340086ff
kdix-23-0240 <2312110240g@kindai.ac.jp>
2025-04-26 18:27:40 +0900

    転生のメソッドを追加(Rebornメソッドは呼ばれているがオブジェクトが生成されない)

--- Assets/Scripts/Flowers/SampleFlowerCreator.cs
+++ Assets/Scripts/Flowers/SampleFlowerCreator.cs
@@ -28,7 +28,7 @@ public class SampleFlowerCreator : MonoBehaviour
         _flowerObjList = new GameObject[_flowerObjNum];
         for (int i = 0; i < _flowerObjNum; i++)
         {
-            Debugger.Log("FlowerObjNumber " + i + " is created");
+            // Debugger.Log("FlowerObjNumber " + i + " is created");
             _flowerObjList[i] = Instantiate(_flowerObj, this.gameObject.transform);
             _flowerObjList[i].transform.localPosition = new Vector3(0, 0, 0);
             _flowerObjList[i].GetComponent<SampleFlowerObj>().Initialize(null, null, null, i);
