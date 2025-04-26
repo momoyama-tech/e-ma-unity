501559dd2d25a7db2266c6b349f6eef3b0a3e41f
kdix-23-0240 <2312110240g@kindai.ac.jp>
2025-04-26 19:27:09 +0900

    Rキーで動物を表示

--- Assets/Scripts/Creator/Animal.cs
+++ Assets/Scripts/Creator/Animal.cs
@@ -2,18 +2,25 @@ using UnityEngine;
 
 public class Animal : MonoBehaviour
 {
-    public void Reborn()
+    private float _goalPosX;
+    public void Initialize(float goalPos)
     {
-
+        _goalPosX = goalPos;
+        gameObject.SetActive(false);
     }
 
-    public void Died()
+    public void ManualUpdate()
     {
-
+        if (Mathf.Abs(gameObject.transform.position.x) <= Mathf.Abs(_goalPosX) - 10f)
+        {
+            Died();
+        }
     }
 
-    public void Move()
+    private void Died()
     {
-
+        gameObject.SetActive(false);
+        // ここに死亡時の処理を追加
+        Debug.Log("Animal Died");
     }
 }
