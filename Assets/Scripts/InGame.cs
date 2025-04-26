d8cd00b616fc4b1479859bc365b2710c57604f31
kdix-23-0240 <2312110240g@kindai.ac.jp>
2025-04-26 19:41:04 +0900

    右からの生成

--- Assets/Scripts/InGame.cs
+++ Assets/Scripts/InGame.cs
@@ -3,19 +3,19 @@ public class InGame : MonoBehaviour
 {
     [SerializeField] private SampleFlowerCreator _flowerCreator;
     [SerializeField] private SampleReborn _rebornLeftSide = null;
-    // [SerializeField] private SampleReborn _rebornRightSide = null;
+    [SerializeField] private SampleReborn _rebornRightSide = null;
 
     void Start()
     {
         _flowerCreator.ManualStart();
         _rebornLeftSide.ManualStart();
-        // _rebornRightSide.ManualStart();
+        _rebornRightSide.ManualStart();
     }
 
     void Update()
     {
         _flowerCreator.ManualUpdate();
         _rebornLeftSide.ManualUpdate();
-        // _rebornRightSide.ManualUpdate();
+        _rebornRightSide.ManualUpdate();
     }
 }
